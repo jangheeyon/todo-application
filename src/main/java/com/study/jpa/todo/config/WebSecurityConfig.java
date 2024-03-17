10081adce4ca99793c97445865433ff90c5e5f76
jangheeyon <dev.hyjang@gmail.com>
2024-03-17 23:49:25 +0900

    :zap: 4.3.2 스프링 시큐리티 설정

---
+++ src/main/java/com/study/jpa/todo/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.study.jpa.todo.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig {
+
+}
